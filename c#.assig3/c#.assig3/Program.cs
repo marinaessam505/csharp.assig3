eaf95e4d0f07c5bec26ef52413a9bf19262f0662
MARINA ESSAM <marinaessam505@gmail.com>
2026-09-08 10:24:44 +0300

    q1

--- c#.assig3/c#.assig3/Program.cs
+++ c#.assig3/c#.assig3/Program.cs
@@ -1,10 +1,58 @@
 ﻿namespace c_.assig3
 {
-    internal class Program
+
+class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+
+            /* Given string title = "Refactoring",
+             write a switch statement
+            that prints "Great choice!" if the title is "Clean Code",
+            "Nice pick!" if it's "Refactoring", 
+            "Never heard of it" for anything else.*/
+
+
+            string title = "Refactoring";
+
+            switch (title)
+            {
+                case "Clean Code":
+                    Console.WriteLine("Great choice!");
+                    break;
+
+                case "Refactoring":
+                    Console.WriteLine("Nice pick!");
+                    break;
+
+                default:
+                    Console.WriteLine("Never heard of it");
+                    break;
+            }
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
+
+
+
 }
